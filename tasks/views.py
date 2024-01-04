1ef7514cf743da61e8d56e3b6924804d362d0c1c
Sam Ordonez <samordonez.ce@gmail.com>
2024-01-04 14:49:10 -0800

    Improved django form functionality

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,13 @@
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django import forms
 from django.shortcuts import render
 
 tasks = ["foo", "bar", "baz"]
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
 # Create your views here.
 def index(request):
@@ -9,4 +16,32 @@ def index(request):
     })
 
 def add(request):
-    return render(request, "tasks/add.html")
+
+    # Check if method is POST
+    if request.method == "POST":
+
+        # Take in the data the user submitted and save it as form
+        form = NewTaskForm(request.POST)
+
+        # Check if form data is valid (server-side)
+        if form.is_valid():
+
+            # Isolate the task from the 'cleaned' version of form data
+            task = form.cleaned_data["task"] #refer to line 9 under class NewTaskForm
+
+            # Add the new task to our list of tasks
+            tasks.append(task)
+
+            # Redirect user to list of tasks
+            return HttpResponseRedirect(reverse("tasks:index"))
+
+        else:
+
+            # If the form is invalid, re-render the page with existing information.
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
