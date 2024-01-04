24fa127dfbdbf38dc265a412682d39d80fc861c0
Sam Ordonez <samsonordonez.ce@gmail.com>
2024-01-04 15:48:26 -0800

    edited index function in tasks>views.py

--- tasks/views.py
+++ tasks/views.py
@@ -11,8 +11,45 @@ class NewTaskForm(forms.Form):
 
 # Create your views here.
 def index(request):
+
+    # Check if there already exists a "tasks" key in our session
+
+    if "tasks" not in request.session:
+
+        # If not, create a new list
+        request.session["tasks"] = []
+
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        "tasks": request.session["tasks"]
+    })
+
+# Add a new task:
+def add(request):
+    if request.method == "POST":
+
+        # Take in the data the user submitted and save it as form
+        form = NewTaskForm(request.POST)
+
+        # Check if form data is valid (server-side)
+        if form.is_valid():
+
+            # Isolate the task from the 'cleaned' version of form data
+            task = form.cleaned_data["task"]
+
+            # Add the new task to our list of tasks
+            request.session["tasks"] += [task]
+
+            # Redirect user to list of tasks
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+
+            # If the form is invalid, re-render the page with existing information.
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
     })
 
 def add(request):
